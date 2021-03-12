24bae8d4634b7bf865d0a7c54a1954b84adb82ba
IrinaDash <irina.lev@list.ru>
2021-03-12 23:29:45 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let brackets = bracketsConfig.map(br => br.join(""));
+
+  console.log(brackets)
+
+  for (let i = 0; i < brackets.length; i++) {
+      console.log(str)
+      if (str.includes(brackets[i])) {
+        str = str.replace(brackets[i], "");
+        i = -1;
+      }
+    }
+  return !str;
 }
